afd7c9e35815d2377fdcbc6ef5f83bae40c45d54
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-07-29 23:29:07 -0400

    started fetch function in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,30 @@
-fetch(
-    'http://www.boredapi.com/api/activity?key=5881028'
-  )
+ var select = document.getElementById('type');
+ var option = select.options[select.selectedIndex].value;
+ 
+
+//var participants = $("#participants").val().trim();
+//var participants = document.getElementById('participants').value;
+console.log(option);
+//var type = document.getElementById("type").value;
+//var urlApi = 'https://www.boredapi.com/api/activity?participants=3';
+var urlApi = 'https://www.boredapi.com/api/activity?type=' + option + "&key=5881028";
+
+$(".btn").click(function (event) {
+
+  fetch(urlApi)
     .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      console.log(data);
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+        });
+    //});
+    // .then(function(data) {
+    //   console.log(data);
+    
+    //   $(".activity").text(data.activity);
+
+    
+    
+    
     });
+  })
