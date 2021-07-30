c1495bd61e1a707ffc39ca50a48249597917f660
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-07-30 00:09:27 -0400

    corrected fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
- var select = document.getElementById('type');
- var option = select.options[select.selectedIndex].value;
+//  var select = document.getElementById('type');
+//  var option = select.options[select.selectedIndex].value;
  
 
 //var participants = $("#participants").val().trim();
 //var participants = document.getElementById('participants').value;
-console.log(option);
-//var type = document.getElementById("type").value;
 //var urlApi = 'https://www.boredapi.com/api/activity?participants=3';
-var urlApi = 'https://www.boredapi.com/api/activity?type=' + option + "&key=5881028";
 
 $(".btn").click(function (event) {
+
+  var type = document.getElementById("type").value;
+  var urlApi = 'https://www.boredapi.com/api/activity?type=' + type;
 
   fetch(urlApi)
     .then(function(response) {
